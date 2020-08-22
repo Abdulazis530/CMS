f4b8c763fd95ba02aaceb4609c4a28ceb14585bf
azis <abdlazis530@gmail.com>
2020-08-22 07:37:05 +0700

    adding method to model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,37 @@
-const mongoose =require('mongoose')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const secret = process.env.TOKEN_SECRET
+
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-    
+    email: {
+        type: String,
+        required: true,
+
     },
-    password:{
-        type:String,
-        required:true,
-        max:100
-    }  
+    password: {
+        type: String,
+        required: true,
+        max: 100
+    }
 })
 
-user
+userSchema.pre("save", async function(next){
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+        this.password = hashedPassword
+        console.log('password succesfully hashed')
+        next();
+    } catch (error) {
+        console.log(error)
+    }
+});
+userSchema.methods.createToken=function(next){
+    const token = jwt.sign({ email:this.email }, secret);
+    next(token)
+}
 
-module.exports=mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', userSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const secret
-
-require('dotenv').config()
-
+const jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 
@@ -16,50 +12,35 @@ router.get('/list', async (req, res) => {
     const result = await User.find({})
     response = result.map(item => {
       return {
-        _id: item._id,
-        email: item.email,
-        password: item.password,
+        _id: item._id, email: item.email, password: item.password,
       }
     })
-
     res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ response });
   }
 })
 
-
-
 router.post('/register', async (req, res, next) => {
   const { email, password, retypepassword } = req.body
-
-  const response = { data: null, token: null, message: "" }
+  const response = { data: {}, token: null, message: "" }
 
   if (password === retypepassword) {
     try {
-      //checking email if already exist in database
       const emailDb = await User.findOne({ email })
-      response.message = `this ${email} already exists`
-      if (emailDb) return res.json(response)
+      response.message = `email already exists`
+      if (emailDb) return res.status(200).json(response)
 
-      //if email !emailDb
-   
-      //hashed password
-      const hashedPassword = await bcrypt.hash(password, saltRounds)
-      //send to db
-      const newUser = new User({ email, password: hashedPassword })
+      const newUser = new User({ email, password })
       await newUser.save()
 
-      //generating token for user
-      const token = jwt.sign({ email }, process.env.TOKEN_SECRET);
-      res.header('auth-token', token).send(token)
-
-      //changing response containing result
-      response.data.email = email
-      response.message = "register success"
-      response.token = token
-
-      res.status(201).json(response)
+      newUser.createToken((token) => {
+        res.header('auth-token', token)
+        response.data.email = email
+        response.message = "register success"
+        response.token = token
+        res.status(201).json(response)
+      })
 
     } catch (error) {
       console.log(error)
@@ -69,11 +50,41 @@ router.post('/register', async (req, res, next) => {
     response.message = "email or password wrong"
     res.status(500).json(response)
   }
-
-
 });
 
 
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body
+  const response = { data: {}, token: null, message: "" }
+
+  if (password === retypepassword) {
+    try {
+
+      const emailDb = await User.findOne({ email })
+      response.message = `email already exists`
+      if (emailDb) return res.status(200).json(response)
+
+
+      const newUser = new User({ email, password })
+      await newUser.save()
+
+      const token = jwt.sign({ email }, secret);
+      res.header('auth-token', token)
+
+      response.data.email = email
+      response.message = "register success"
+      response.token = token
+
+      res.status(201).json(response)
+    } catch (error) {
+      console.log(error)
+      res.status(400).json(response)
+    }
+  } else {
+    response.message = "email or password wrong"
+    res.status(500).json(response)
+  }
+});
 router.put('/:id', async (req, res, next) => {
   const { name, phone } = req.body
 
@@ -92,7 +103,7 @@ router.delete('/:id', async (req, res, next) => {
 
   try {
 
-    const data = await Phonebook.findByIdAndRemove(req.params.id)
+    const data = await User.findByIdAndRemove(req.params.id)
     res.status(201).json(data)
 
   } catch (error) {
