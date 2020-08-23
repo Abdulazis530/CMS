ede23bbb3809d4c16a94b1dd418e5df2486eb105
azis <abdlazis530@gmail.com>
2020-08-23 21:50:09 +0700

    refactoring auth.test.js using syntaxtical sugar async/awai

--- test/auth.test.js
+++ test/auth.test.js
@@ -8,129 +8,114 @@ const User = require('../models/User')
 const should = chai.should()
 chai.use(chaiHTTP)
 
-describe('users', function () {
+describe('users', () => {
   User.collection.drop();
 
-  beforeEach(function (done) {
+  beforeEach(async () => {
     let user = new User({
       email: 'abdlazis530@gmail.com',
       password: 'kucinganggoralapar',
       token: "",
     });
-    user.save(function (err) {
-      done();
-    })
+    await user.save();
   })
 
-  afterEach(function (done) {
-    User.collection.drop();
-    done()
+  afterEach(async () => {
+    await User.collection.drop();
+
   })
 
-  it('should list ALL users on /api/users/list GET', function (done) {
-    chai.request(server)
-      .get('/api/users/list')
-      .end(function (err, res) {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body[0].should.have.property('_id');
-        res.body[0].should.have.property('email');
-        res.body[0].should.have.property('password');
-        res.body[0].should.have.property('token');
-        res.body[0].email.should.equal('abdlazis530@gmail.com');
-        done();
-      });
+  it('should list ALL users on /api/users/list GET', async () => {
+    const data = await chai.request(server).get('/api/users/list');
+    data.should.have.status(200);
+    data.body.should.be.a('array');
+    data.body[0].should.have.property('_id');
+    data.body[0].should.have.property('email');
+    data.body[0].should.have.property('password');
+    data.body[0].should.have.property('token');
+    data.body[0].email.should.equal('abdlazis530@gmail.com');
+
   });
 
-  it('should add a SINGLE user on /api/users/register POST', function (done) {
-    chai.request(server)
+  it('should add a SINGLE user on /api/users/register POST', async () => {
+    const data = await chai.request(server)
       .post('/api/users/register')
-      .send({ 'email': 'ladybug@gmail.com', 'password': '12345', 'retypepassword': '12345' })
-      .end(function (err, res) {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('data');
-        res.body.should.have.property('token');
-        res.body.should.have.property('message');
-        expect(res.body.token).to.exist;
-        res.body.data.email.should.equal('ladybug@gmail.com');
-        res.body.message.should.equal('register success');
-        done();
-      });
+      .send({ 'email': 'ladybug@gmail.com', 'password': '12345', 'retypepassword': '12345' });
+
+    data.should.have.status(201);
+    data.should.be.json;
+    data.body.should.be.a('object');
+    data.body.should.have.property('data');
+    data.body.should.have.property('token');
+    data.body.should.have.property('message');
+    expect(data.body.token).to.exist;
+    data.body.data.email.should.equal('ladybug@gmail.com');
+    data.body.message.should.equal('register success');
+
   });
 
-  it('should LOGIN into /api/users/login POST', function (done) {
-    chai.request(server)
+  it('should LOGIN into /api/users/login POST', async () => {
+    const data = await chai.request(server)
       .post('/api/users/login')
-      .send({ 'email': 'abdlazis530@gmail.com', 'password': 'kucinganggoralapar' })
-      .end(function (err, res) {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('data');
-        res.body.should.have.property('token');
-        res.body.should.have.property('message');
-        expect(res.body.token).to.exist;
-        res.body.data.email.should.equal('abdlazis530@gmail.com');
-        res.body.message.should.equal('login success');
-        done();
+      .send({
+        'email': 'abdlazis530@gmail.com',
+        'password': 'kucinganggoralapar'
       });
+
+    data.should.have.status(201);
+    data.should.be.json;
+    data.body.should.be.a('object');
+    data.body.should.have.property('data');
+    data.body.should.have.property('token');
+    data.body.should.have.property('message');
+    expect(data.body.token).to.exist;
+    data.body.data.email.should.equal('abdlazis530@gmail.com');
+    data.body.message.should.equal('login success');
+
   });
 
+  it('should Check token user when access /api/users/check POST', async () => {
+    const dataLogin = await chai
+      .request(server)
+      .post("/api/users/login")
+      .send({
+        'email': "abdlazis530@gmail.com",
+        'password': "kucinganggoralapar"
+      })
+    const token = dataLogin.body.token;
 
-  it('should Check token user when access /api/users/check POST', function (done) {
-    chai
-    .request(server)
-    .post("/api/users/login")
-    .send({
-      email: "abdlazis530@gmail.com",
-      password: "kucinganggoralapar"
-    })
-    .end((err, response) => {
-      const token = response.body.token;
-      console.log('REQ BODY:',response.body)
-      console.log('TOKEN TES:',token)
-      chai
-        .request(server)
-        .post("/api/users/check")
-        .set( "Authorization",token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a("object");
-          res.body.should.have.property("valid");
-          res.body.valid.should.equal(true);
-          done();
-        });
-    });
+    const dataCheck = await chai
+      .request(server)
+      .post("/api/users/check")
+      .set("Authorization", token)
+
+    dataCheck.should.have.status(200);
+    dataCheck.should.be.json;
+    dataCheck.body.should.be.a("object");
+    dataCheck.body.should.have.property("valid");
+    dataCheck.body.valid.should.equal(true);
   });
 
-  it('should logout when access /api/users/destroy GET', function (done) {
-    chai
-    .request(server)
-    .post("/api/users/login")
-    .send({
-      email: "abdlazis530@gmail.com",
-      password: "kucinganggoralapar"
-    })
-    .end((err, response) => {
-      const token = response.body.token;
-      console.log('REQ BODY:',response.body)
-      console.log('TOKEN TES:',token)
-      chai
-        .request(server)
-        .get("/api/users/destroy")
-        .set( "Authorization",token)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a("object");
-          res.body.should.have.property("logout");
-          res.body.logout.should.equal(true);
-          done();
-        });
-    });
+  it('should logout when access /api/users/destroy GET', async () => {
+    const dataLogin = await chai
+      .request(server)
+      .post("/api/users/login")
+      .send({
+        email: "abdlazis530@gmail.com",
+        password: "kucinganggoralapar"
+      })
+
+    const token = dataLogin.body.token;
+    const dataLogout = await chai
+      .request(server)
+      .get("/api/users/destroy")
+      .set("Authorization", token)
+    dataLogout.should.have.status(200);
+    dataLogout.should.be.json;
+    dataLogout.body.should.be.a("object");
+    dataLogout.body.should.have.property("logout");
+    dataLogout.body.logout.should.equal(true);
+
   });
 
 })
