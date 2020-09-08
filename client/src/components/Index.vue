380b45039ff55e030d783d0298741ad2dceb6adc
azis <abdlazis530@gmail.com>
2020-09-08 23:34:57 +0700

    Styling log in

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,92 +1,95 @@
 <template>
-  <div>
-    <div class="jumbotron jumbotron-fluid mb-5">
-      <div class="container p-2">
-        <h1 class="display-3 text-white">Content Management System</h1>
+  <div >
+    <Navbar></Navbar>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-3 text-success">Content Management System</h1>
         <p
-          class="lead text-white"
+          class="lead text-dark"
         >This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
       </div>
     </div>
 
-<!-- START OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
-    <div class="container-card container d-flex flex-column justify-content-center">
-      <div class="card mb-3">
+    <!-- START OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
+    <div class="container-card container">
+      <div class="card mb-3 justify-content-center w-75">
         <div class="card-header">Welcome to Dashboard</div>
         <div class="card-body text-dark">
-          <div class="d-flex flex-column bd-highlight w-100">
-
-            <a class="childTomato p-2 bd-highlight">
-              <i class="fas fa-signal fa-2x"></i>
+          <div class="d-flex flex-column bd-highlight">
+            <a class="childTomato p-1 bd-highlight">
+              <i class="fas fa-signal fa-sm"></i>
               <br />
               <span>
                 <strong>Line</strong>
               </span>
             </a>
 
-            <a class="childYellow p-2 mt-2 bd-highlight">
-              <i class="fas fa-chart-pie fa-2x"></i>
+            <a class="childYellow p-1 mt-2 bd-highlight">
+              <i class="fas fa-chart-pie fa-sm"></i>
               <br />
               <span>
                 <strong>Pie</strong>
               </span>
             </a>
 
-            <a class="childAqua p-2 mt-2 bd-highlight">
-              <i class="far fa-chart-bar fa-2x"></i>
+            <a class="childAqua p-1 mt-2 bd-highlight">
+              <i class="far fa-chart-bar fa-sm"></i>
               <br />
               <span>
                 <strong>Bar</strong>
               </span>
             </a>
 
-            <a class="childGreen p-2 mt-2 bd-highlight">
-              <i class="fas fa-map-marked-alt fa-2x"></i>
-             <br/>
+            <a class="childGreen p-1 mt-2 bd-highlight">
+              <i class="fas fa-map-marked-alt fa-sm"></i>
+              <br />
               <span>
                 <strong>Maps</strong>
               </span>
-              </a>
+            </a>
           </div>
 
-          <div class="d-flex flex-row bd-highlight justify-content-center">
-            <div class="p-3 mt-2 mr-2 bd-highlight btn btn-success">
-              <i class="fas fa-signal"></i>
-              <span>Flex item 1</span>
-            </div>
-            <div class="flexChild p-3 mt-2 ml-2 bd-highlight">Flex item 2</div>
+          <div class="d-flex flex-row mt-5 bd-highlight justify-content-center text-black-50">
+            <router-link to="/Login" class="flexChild p-2 ml-2 btn btn-success w-25">Login</router-link>
           </div>
         </div>
       </div>
     </div>
-<!-- END OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
-
+    <!-- END OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
   </div>
 </template>
 
 <script>
+import Navbar from "./Navbar.vue";
+
 export default {
-  name: "firstView",
-  props: {
-    msg: String,
-  },
+  name:'Index',
+  components: { Navbar }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .jumbotron {
-  background-color: #09644c;
+  background-image: url("../assets/background2.jpg");
+  background-position: center bottom fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+
+  background-color: rgba(255, 255, 255, 0.8);
   box-shadow: 0px 5px 10px rgba(1, 5, 1, 0.6);
 }
+
 .card {
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: rgba(255, 255, 255, 0.8);
   box-shadow: 0 5px 10px 2px rgba(0, 0, 0, 0.6);
+  margin-left: 12%;
 }
 .flexChild {
-  background-color: rgb(83, 192, 140, 1);
   transition: transform 0.2s;
-  border-radius: 10px;
   font-size: 1.5em;
   box-shadow: 0 10px 10px rgba(0, 0, 0, 0.6);
 }
@@ -94,7 +97,7 @@ export default {
 .childYellow,
 .childAqua,
 .childTomato {
-  border-radius: 10px;
+  border-radius: 8px;
   font-size: 1.5em;
   box-shadow: 0 10px 10px rgba(0, 0, 0, 0.6);
   transition: transform 0.2s;
@@ -128,9 +131,8 @@ export default {
   transform: scale(1.02);
 }
 .card-header {
-  background-color: rgba(0, 0, 0, 0.6);
-
-  color: white;
+  background-color: rgba(255, 255, 255, 0.5);
+  color: black;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 2rem;
   font-weight: bold;
@@ -145,7 +147,7 @@ export default {
   background-color: red;
 }
 .container-card {
-  margin-top: 10vh;
+  margin-top: 5vh;
 }
 h3 {
   margin: 40px 0 0;
@@ -159,9 +161,9 @@ li {
   margin: 0 10px;
 }
 a {
-  color: rgb(53,50,50);
+  color: rgb(53, 50, 50);
 }
-a:hover{
+a:hover {
   color: white;
 }
 </style>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <!-- START OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
+    <div class="container-card container">
+      <div class="card mb-3 ">
+        <div class="card-header">Login to CMS</div>
+        <div class="card-body text-dark">
+          <form class="d-flex flex-column align-items-center" >
+            <div class="p-2 bd-highligh w-50">
+              <label class=" text-white">Email</label>
+              <input class="form-control form-control-lg" type="Email" placeholder="Email"  required/>
+            </div>
+             <div class="p-2 bd-highligh w-50">
+              <label class=" text-white">Password</label>
+              <input class="form-control form-control-lg" type="Password" placeholder="Password"  required/>
+            </div>
+          
+            
+          </form>
+
+          <div class="d-flex flex-row mt-5 bd-highlight justify-content-center text-black-50">
+            <router-link to="/Login" class="flexChild p-2 ml-2 btn btn-success w-25">Login</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- END OF SELECTION FEATURE(LINE,PIE,BAR,MAPS) -->
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+export default {
+  name: "login",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.jumbotron {
+  background-image: url("../assets/background2.jpg");
+  background-position: center bottom fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+
+  background-color: rgba(255, 255, 255, 0.8);
+  box-shadow: 0px 5px 10px rgba(1, 5, 1, 0.6);
+}
+
+.card {
+  background-color: rgba(0, 0, 0, 0.4);
+  box-shadow: 0 5px 10px 2px rgba(0, 0, 0, 0.6);
+  margin-left: 12%;
+}
+.flexChild {
+  transition: transform 0.2s;
+  font-size: 1.5em;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.6);
+}
+.childGreen,
+.childYellow,
+.childAqua,
+.childTomato {
+  border-radius: 8px;
+  font-size: 1.5em;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.6);
+  transition: transform 0.2s;
+}
+.childGreen {
+  background-color: rgb(83, 192, 140, 1);
+}
+.childGreen:hover {
+  background-color: rgba(83, 192, 140, 0.6);
+  transform: scale(1.02);
+}
+.childYellow {
+  background-color: rgba(251, 212, 109, 1);
+}
+.childYellow:hover {
+  background-color: rgba(251, 212, 109, 0.6);
+  transform: scale(1.02);
+}
+.childAqua {
+  background-color: rgba(29, 211, 189, 1);
+}
+.childAqua:hover {
+  background-color: rgba(29, 211, 189, 0.6);
+  transform: scale(1.02);
+}
+.childTomato {
+  background-color: rgba(160, 27, 27, 1);
+}
+.childTomato:hover {
+  background-color: rgba(207, 27, 27, 0.6);
+  transform: scale(1.02);
+}
+.card-header {
+  background-color: rgba(0, 0, 0, 0.6);
+
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2rem;
+  font-weight: bold;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.6);
+}
+.flexChild:hover {
+  background-color: rgba(83, 192, 140, 0.6);
+  transform: scale(1.02);
+}
+/* 53C08C */
+.flexChild1 {
+  background-color: red;
+}
+.container-card {
+  margin-top: 5vh;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: rgb(53, 50, 50);
+}
+a:hover {
+  color: white;
+}
+</style>
